2e47760ba33f019987d28c2504fdd11ea65f8e9b
phquartin <phquartinmatos@sempreceub.com>
2025-08-12 21:33:44 -0300

    feat(Category): adicionados mapeamentos para entidade Category

---
+++ src/main/java/dev/phquartin/storesalescleanapi/infra/mapper/CategoryMapper.java
@@ -0,0 +1,19 @@
+package dev.phquartin.storesalescleanapi.infra.mapper;
+
+import dev.phquartin.storesalescleanapi.core.domain.Category;
+import dev.phquartin.storesalescleanapi.infra.presentation.request.CategoryRequest;
+import dev.phquartin.storesalescleanapi.infra.presentation.response.CategoryResponse;
+
+public class CategoryMapper {
+
+    public Category toDomain(CategoryRequest request) {
+        return new Category(null, request.name());
+    }
+
+    public CategoryResponse toResponse (Category entity) {
+        return CategoryResponse.builder()
+                .name(entity.name())
+                .build();
+    }
+
+}
