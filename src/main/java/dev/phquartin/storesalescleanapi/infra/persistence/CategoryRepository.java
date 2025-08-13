74d91ddfbeb772db69ae49a1de786a4e7af75be9
phquartin <phquartinmatos@sempreceub.com>
2025-08-12 21:08:35 -0300

    feat(persistence): adicionados repositórios de Category e Product

---
+++ src/main/java/dev/phquartin/storesalescleanapi/infra/persistence/CategoryRepository.java
@@ -0,0 +1,8 @@
+package dev.phquartin.storesalescleanapi.infra.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
+}
