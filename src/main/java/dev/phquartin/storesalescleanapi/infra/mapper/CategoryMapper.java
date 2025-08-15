f8ee4233607398611f77d15f946f4f175f27d22d
phquartin <phquartinmatos@sempreceub.com>
2025-08-15 15:52:29 -0300

    feat(Category): adicionado `@Component` annotation em `CategoryMapper`

--- src/main/java/dev/phquartin/storesalescleanapi/infra/mapper/CategoryMapper.java
+++ src/main/java/dev/phquartin/storesalescleanapi/infra/mapper/CategoryMapper.java
@@ -3,7 +3,9 @@ package dev.phquartin.storesalescleanapi.infra.mapper;
 import dev.phquartin.storesalescleanapi.core.domain.Category;
 import dev.phquartin.storesalescleanapi.infra.presentation.request.CategoryRequest;
 import dev.phquartin.storesalescleanapi.infra.presentation.response.CategoryResponse;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CategoryMapper {
 
     public Category toDomain(CategoryRequest request) {
