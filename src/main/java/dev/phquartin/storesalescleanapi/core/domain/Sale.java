544d1c21d05db4d89e56948ebab12649951b0049
phquartin <phquartinmatos@sempreceub.com>
2025-08-17 16:48:06 -0300

    refactor(Sale): refatorados domínios `Sale` e `SaleItens` de `record` para classes com métodos utilitários para cálculos.

--- src/main/java/dev/phquartin/storesalescleanapi/core/domain/Sale.java
+++ src/main/java/dev/phquartin/storesalescleanapi/core/domain/Sale.java
@@ -5,11 +5,62 @@ import dev.phquartin.storesalescleanapi.core.enums.PaymentMethod;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public record Sale(
-        Long id,
-        List<SaleItens> itens,
-        LocalDateTime dateTime,
-        Double totalValue,
-        PaymentMethod paymentMethod
-) {
+public class Sale {
+    private Long id;
+    private List<SaleItens> itens;
+    private LocalDateTime dateTime;
+    private Double totalValue;
+    private PaymentMethod paymentMethod;
+
+    public Sale(Long id, List<SaleItens> itens, LocalDateTime dateTime, PaymentMethod paymentMethod) {
+        this.id = id;
+        this.itens = itens;
+        this.dateTime = dateTime;
+        this.paymentMethod = paymentMethod;
+        this.totalValue = calculateTotalValue();
+    }
+
+    private Double calculateTotalValue() {
+        return this.itens.stream().mapToDouble(SaleItens::getPrice).sum();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<SaleItens> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<SaleItens> itens) {
+        this.itens = itens;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public Double getTotalValue() {
+        return totalValue;
+    }
+
+    public void setTotalValue(Double totalValue) {
+        this.totalValue = totalValue;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
 }
--- src/main/java/dev/phquartin/storesalescleanapi/core/domain/SaleItens.java
+++ src/main/java/dev/phquartin/storesalescleanapi/core/domain/SaleItens.java
@@ -1,11 +1,64 @@
 package dev.phquartin.storesalescleanapi.core.domain;
 
 
-public record SaleItens(
-        Long id,
-        Product product,
-        int quantity,
-        Double price,
-        Sale sale
-        ) {
+public class SaleItens{
+        private Long id;
+        private Product product;
+        private int quantity;
+        private Double price;
+        private Sale sale;
+
+        public SaleItens(Long id, Product product, int quantity, Sale sale) {
+                this.id = id;
+                this.product = product;
+                this.quantity = quantity;
+                this.sale = sale;
+                this.price = calculateParcialValue();
+        }
+
+
+        private Double calculateParcialValue() {
+                return this.quantity * this.product.price();
+
+        }
+
+        public Long getId() {
+                return id;
+        }
+
+        public Product getProduct() {
+                return product;
+        }
+
+        public int getQuantity() {
+                return quantity;
+        }
+
+        public Double getPrice() {
+                return price;
+        }
+
+        public Sale getSale() {
+                return sale;
+        }
+
+        public void setId(Long id) {
+                this.id = id;
+        }
+
+        public void setProduct(Product product) {
+                this.product = product;
+        }
+
+        public void setQuantity(int quantity) {
+                this.quantity = quantity;
+        }
+
+        public void setPrice(Double price) {
+                this.price = price;
+        }
+
+        public void setSale(Sale sale) {
+                this.sale = sale;
+        }
 }
